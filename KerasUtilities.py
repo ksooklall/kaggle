b8196f39b5b8876c4a2fa13a3de7b36e3271ef99
ksooklall <kensprogramming42@gmail.com>
2017-10-16 23:36:07 -0400

    autoencoder needs more work

--- KerasUtilities.py
+++ KerasUtilities.py
@@ -5,10 +5,11 @@ from keras.utils import np_utils
 from keras.models import Sequential, Model, Input
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.layers import Dense, BatchNormalization, Dropout
+
 import matplotlib.pyplot as plt
 
 
-def vanilla_nn(X_train, y_train, hidden_units, drop_out, batch_size, depth):
+def vanilla_nn(X_train, y_train, hidden_units, drop_out, batch_size, depth, loss, filepath):
     """
     A feed_foward network
     S_order(2)
@@ -18,6 +19,18 @@ def vanilla_nn(X_train, y_train, hidden_units, drop_out, batch_size, depth):
     :param depth:
     :return:
     """
+    if not isinstance(hidden_units, int):
+        raise ValueError('Hidden units must be an int, found {}'.format(hidden_units))
+
+    if not isinstance(drop_out, float):
+        raise ValueError('Hidden units must be an int, found {}'.format(drop_out))
+
+    if not isinstance(batch_size, int):
+        raise ValueError('Hidden units must be an int, found {}'.format(batch_size))
+
+    if not isinstance(depth, int):
+        raise ValueError('Hidden units must be an int, found {}'.format(depth))
+
     model = Sequential()
     regularizers.l2(0.01)
     trunc_norm = initializers.truncated_normal(stddev=0.01)
@@ -30,33 +43,37 @@ def vanilla_nn(X_train, y_train, hidden_units, drop_out, batch_size, depth):
 
     model.add(Dense(hidden_units//2, kernel_initializer=trunc_norm, activation='relu'))
     model.add(Dropout(drop_out))
-    model.add(Dense(1))
+    model.add(Dense(y_train.shape[-1], activation='softmax'))
 
     adam = optimizers.Adam(lr=0.001)
-    model.compile(loss='mean_squared_error', optimizer=adam, metrics=['accuracy'])
+    model.compile(loss=loss, optimizer=adam, metrics=['accuracy'])
     model.summary()
 
-    checkpointer = ModelCheckpoint(filepath='model.weights.best.h5', verbose=1, save_best_only=True)
-    model_info = model.fit(X_train, y_train, batch_size=batch_size, epochs=100, validation_split=0.2, callbacks=[checkpointer], verbose=1)
+    checkpointer = ModelCheckpoint(filepath=filepath, verbose=1, save_best_only=True)
+    model_dict = model.fit(X_train, y_train, batch_size=batch_size, epochs=25, validation_split=0.2,
+                           callbacks=[checkpointer], verbose=1).history
+    plot_model(model_dict)
+    return model, model_dict
 
-    return model, model_info
 
-
-def plot_model(model):
+def plot_model(model_dict):
     """
     Plotting graph
     :param model: Keras model
     :return:
     """
     fig, ax = plt.subplots(1, 2, figsize=(10,5))
-    ax[0].plot(range(1, len(model.history['acc']) + 1), model.history['acc'])
-    ax[0].plot(range(1, len(model.history['val_acc']) + 1), model.history['val_acc'])
+    ax[0].plot(range(1, len(model_dict['acc']) + 1), model_dict['acc'])
+    ax[0].plot(range(1, len(model_dict['val_acc']) + 1), model_dict['val_acc'])
     ax[0].set_title('Model Accuracy')
     ax[0].set_ylabel('Accuracy')
     ax[0].set_xlabel('Epoch')
+    ax[0].legend()
 
-    ax[1].plot(range(1, len(model.history['loss']) + 1), model.history['loss'])
-    ax[1].plot(range(1, len(model.history['val_loss']) + 1), model.history['val_loss'])
+    ax[1].plot(range(1, len(model_dict['loss']) + 1), model_dict['loss'])
+    ax[1].plot(range(1, len(model_dict['val_loss']) + 1), model_dict['val_loss'])
     ax[1].set_title('Model Loss')
     ax[1].set_ylabel('Loss')
     ax[1].set_xlabel('Epoch')
+    ax[1].legend()
+    plt.show()
--- Zillow_home/zillow_model.py
+++ Zillow_home/zillow_model.py
@@ -23,7 +23,7 @@ def add_date_features(df):
 
 def clean_columns(df):
     df.columns = map(str.lower, df.columns)
-    id_col = [col for col in df.columns if 'id' in col]
+    id_col = [col for col in df.columns if ('id' in col) and (col != 'parcelid')]
     df = df.drop(id_col, axis=1)
     return df
 
@@ -67,18 +67,9 @@ def model_art(X, y):
 train_df = add_date_features(train_df)
 train_df = train_df.merge(df, how='left', on='parcelid')
 test_df = test_df.merge(df, how='left', on='parcelid')
+df = pd.concat([train_df, test_df], axis=0)
 
-#train_df = filter_cols_by_nan(train_df)
-train_df = clean_columns(train_df)
-train_df = train_df .drop(['transcation_year', 'censustractandblock'], axis=1)
-train_df = train_df[~train_df.duplicated()]
-
-test_df['transactiondate'] = pd.Timestamp('2016-12-01')
-test_df = add_date_features(test_df)
-# Fill with large value
-train_df = train_df.fillna(-1)
-test_df = test_df.fillna(-1)
-
+"""
 train_features = ['transcation_month', 'transcation_day', 'transcation_quarter',
                   'bathroomcnt', 'bedroomcnt', 'calculatedbathnbr',
                   'finishedfloor1squarefeet', 'calculatedfinishedsquarefeet',
@@ -89,65 +80,88 @@ train_features = ['transcation_month', 'transcation_day', 'transcation_quarter',
                   'unitcnt', 'yardbuildingsqft17', 'yearbuilt',
                   'numberofstories', 'structuretaxvaluedollarcnt', 'taxvaluedollarcnt',
                   'assessmentyear', 'landtaxvaluedollarcnt', 'taxamount']
+"""
 
-df = train_df
-df['N-life'] = 2018 - df['yearbuilt']
-df['A-calculatedfinishedsquarefeet'] = df['finishedsquarefeet12'] + df['finishedsquarefeet15']
+def addition_features(df):
+    df['N-life'] = 2018 - df['yearbuilt']
+    df['A-calculatedfinishedsquarefeet'] = df['finishedsquarefeet12'] + df['finishedsquarefeet15']
 
-# error in calculation of the finished living area of home
-df['N-LivingAreaError'] = df['calculatedfinishedsquarefeet'] / df['finishedsquarefeet12']
+    # error in calculation of the finished living area of home
+    df['N-LivingAreaError'] = df['calculatedfinishedsquarefeet'] / df['finishedsquarefeet12']
 
-# proportion of living area
-df['N-LivingAreaProp'] = df['calculatedfinishedsquarefeet'] / df['lotsizesquarefeet']
-df['N-LivingAreaProp2'] = df['finishedsquarefeet12'] / df['finishedsquarefeet15']
+    # proportion of living area
+    df['N-LivingAreaProp'] = df['calculatedfinishedsquarefeet'] / df['lotsizesquarefeet']
+    df['N-LivingAreaProp2'] = df['finishedsquarefeet12'] / df['finishedsquarefeet15']
 
-# Amout of extra space
-df['N-ExtraSpace'] = df['lotsizesquarefeet'] - df['calculatedfinishedsquarefeet']
-df['N-ExtraSpace-2'] = df['finishedsquarefeet15'] - df['finishedsquarefeet12']
+    # Amout of extra space
+    df['N-ExtraSpace'] = df['lotsizesquarefeet'] - df['calculatedfinishedsquarefeet']
+    df['N-ExtraSpace-2'] = df['finishedsquarefeet15'] - df['finishedsquarefeet12']
 
-# Total number of rooms
-df['N-TotalRooms'] = df['bathroomcnt'] + df['bedroomcnt']
+    # Total number of rooms
+    df['N-TotalRooms'] = df['bathroomcnt'] + df['bedroomcnt']
 
-# Average room size
-#df['N-AvRoomSize'] = df['calculatedfinishedsquarefeet'] / df['roomcnt']
+    # Average room size
+    #df['N-AvRoomSize'] = df['calculatedfinishedsquarefeet'] / df['roomcnt']
 
-# Number of Extra rooms
-df['N-ExtraRooms'] = df['roomcnt'] - df['N-TotalRooms']
+    # Number of Extra rooms
+    df['N-ExtraRooms'] = df['roomcnt'] - df['N-TotalRooms']
 
-# Ratio of the built structure value to land area
-df['N-ValueProp'] = df['structuretaxvaluedollarcnt'] / df['landtaxvaluedollarcnt']
+    # Ratio of the built structure value to land area
+    df['N-ValueProp'] = df['structuretaxvaluedollarcnt'] / df['landtaxvaluedollarcnt']
 
-# Does property have a garage, pool or hot tub and AC?
-#df['N-GarPoolAC'] = ((df['garagecarcnt'] > 0) & (df['pooltypeid10'] > 0) & (df['airconditioningtypeid'] != 5)) * 1
+    # Does property have a garage, pool or hot tub and AC?
+    #df['N-GarPoolAC'] = ((df['garagecarcnt'] > 0) & (df['pooltypeid10'] > 0) & (df['airconditioningtypeid'] != 5)) * 1
 
-df["N-location"] = df["latitude"] + df["longitude"]
-df["N-location-2"] = df["latitude"] * df["longitude"]
-#df["N-location-2round"] = df["N-location-2"].round(-4)
+    df["N-location"] = df["latitude"] + df["longitude"]
+    df["N-location-2"] = df["latitude"] * df["longitude"]
+    #df["N-location-2round"] = df["N-location-2"].round(-4)
 
-# Ratio of tax of property over parcel
-df['N-ValueRatio'] = df['taxvaluedollarcnt'] / df['taxamount']
+    # Ratio of tax of property over parcel
+    df['N-ValueRatio'] = df['taxvaluedollarcnt'] / df['taxamount']
 
-# TotalTaxScore
-df['N-TaxScore'] = df['taxvaluedollarcnt'] * df['taxamount']
+    # TotalTaxScore
+    df['N-TaxScore'] = df['taxvaluedollarcnt'] * df['taxamount']
 
-# polnomials of tax delinquency year
-df["N-taxdelinquencyyear-2"] = df["taxdelinquencyyear"] ** 2
-df["N-taxdelinquencyyear-3"] = df["taxdelinquencyyear"] ** 3
+    # polnomials of tax delinquency year
+    df["N-taxdelinquencyyear-2"] = df["taxdelinquencyyear"] ** 2
+    df["N-taxdelinquencyyear-3"] = df["taxdelinquencyyear"] ** 3
 
-# Length of time since unpaid taxes
-df['N-live'] = 2018 - df['taxdelinquencyyear']
+    # Length of time since unpaid taxes
+    df['N-live'] = 2018 - df['taxdelinquencyyear']
+    return df
+"""
+train_df = clean_columns(train_df)
+train_df = train_df .drop(['transcation_year', 'censustractandblock'], axis=1)
+train_df = train_df[~train_df.duplicated()]
 
-train_df = filter_outliers(df, 'logerror')
-train_df = train_df[[col for col in train_df.columns if 'N' in col] + ['logerror']]
+test_df['transactiondate'] = pd.Timestamp('2016-12-01')
+test_df = add_date_features(test_df)
+# Fill with large value
+train_df = train_df.fillna(-1)
+test_df = test_df.fillna(-1)
+"""
+
+#df = filter_outliers(df, 'logerror')
+df = filter_cols_by_nan(df, 0.98)
+trdf = train_df.merge(df, how='left', on='parcelid')
+trdf = trdf[~trdf['parcelid'].duplicated()]
+
+tedf = test_df.merge(df, how='left', on='parcelid')
+tedf = tedf[~tedf['parcelid'].duplicated()]
+# Get train_df and test_df
+
+import pdb; pdb.set_trace()
+train_cols = set([col.split('_')[0] for col in trdf.columns if '20' not in col])
+train_df = filter_outliers(trdf, 'logerror')
+train_df = train_df.drop(['logerror'], axis=1)
 
 X_train = np.array(train_df, dtype=np.float32)
 y_train = np.array(train_df['logerror'], dtype=np.float32)
 
-import pdb; pdb.set_trace()
 X_test = np.array(test_df[train_df.columns], dtype=np.float32)
 
-#model, model_history = vanilla_nn(X_train, y_train, 1024, 0.3, 256, 4)
-model = model_art(X_train, y_train)
+model, model_history = vanilla_nn(X_train, y_train, 1024, 0.3, 256, depth=4)
+#model = model_art(X_train, y_train)
 model.load_weights('model.weights.best.h5')
 y_pred = model.predict(X_test)
 pdb.set_trace()
@@ -156,6 +170,6 @@ submission = pd.DataFrame({'ParcelId': test_df['parcelid'].astype(np.int32),
                            '201610': y_pred, '201611': y_pred, '201612': y_pred,
                            '201710': y_pred, '201711': y_pred, '201712': y_pred})
 pdb.set_trace()
-BUILD_COUNT = 2
+BUILD_COUNT = 3
 submission.to_csv('submission_{}.csv'.format(BUILD_COUNT), float_format='%.4f', index=False)
 
